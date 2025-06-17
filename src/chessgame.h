e08432dfcc46d2e2a861dd083c75be44c0438412
chim-yun <kms04kms09@gmail.com>
2025-06-17 12:05:11 +0900

    Expand Qt chess prototype

---
+++ src/chessgame.h
@@ -0,0 +1,46 @@
+#pragma once
+#include <QWidget>
+#include <QVector>
+#include <QPair>
+#include <QTimer>
+#include <QTime>
+
+enum class PieceType { None, Pawn, Knight, Bishop, Rook, Queen, King };
+enum class Color { White, Black };
+
+struct Piece {
+    PieceType type;
+    Color color;
+};
+
+class ChessGame : public QWidget {
+    Q_OBJECT
+public:
+    explicit ChessGame(bool vsAI = false, Color aiColor = Color::Black, QWidget* parent = nullptr);
+    QSize sizeHint() const override { return QSize(512,512); }
+
+signals:
+    void gameOver();
+
+private:
+    QVector<Piece> board;
+    Color turn;
+    bool vsAI;
+    Color aiColor;
+    QTime whiteTime;
+    QTime blackTime;
+    QTimer tick;
+    int selected;
+    QVector<int> highlights;
+
+    void setupBoard();
+    void loadAI();
+    QVector<int> validMoves(int idx) const;
+    void movePiece(int from, int to);
+    void checkTimers();
+    void endGame(const QString& winner);
+protected:
+    void paintEvent(QPaintEvent*) override;
+    void mousePressEvent(QMouseEvent*) override;
+};
+
---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <QApplication>
+#include "login.h"
+#include "chessgame.h"
+#include "startdialog.h"
+#include <memory>
+#include <functional>
+
+int main(int argc, char** argv) {
+    QApplication app(argc, argv);
+    LoginDialog login;
+    if (login.exec() != QDialog::Accepted)
+        return 0;
+
+    std::function<void()> startGame;
+    std::shared_ptr<ChessGame> game;
+    startGame = [&]() {
+        StartDialog dlg;
+        if (dlg.exec() != QDialog::Accepted) { app.quit(); return; }
+        bool vsAI = dlg.mode() == GameMode::VsAI;
+        game.reset(new ChessGame(vsAI, dlg.aiColor()));
+        QObject::connect(game.get(), &ChessGame::gameOver, startGame);
+        game->show();
+    };
+
+    startGame();
+    return app.exec();
+}
